5c5d2d80d23120b2b44c34a3b13d9c630249f6d4
Drikus Roor <drikusroor@gmail.com>
2022-09-25 12:03:11 +0200

    Format examples

--- docs/.vuepress/components/HomePageDemo.vue
+++ docs/.vuepress/components/HomePageDemo.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="pl-1 pb-2 pt-3">Selected Country: {{query}}</div>
     <vue-bootstrap-autocomplete
+      class="mb-4"
       :data="['Canada', 'United Kingdom', 'United States', 'Mexico', 'Netherlands']"
       v-model="query"
       showOnFocus
@@ -17,7 +18,7 @@
   export default {
     name: "HomePageDemo",
     components: {VueBootstrapAutocomplete},
-    data(){
+    data() {
       return {
         query: ''
       }
