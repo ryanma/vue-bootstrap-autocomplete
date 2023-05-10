7d59620573b0e65b42fe984596001b75ca7929c1
Pete <60928397+pager-itc@users.noreply.github.com>
2023-05-10 21:15:02 +0100

    fix: Select list items on enter keypress (#11)

--- src/components/VueBootstrapAutocompleteListItem.vue
+++ src/components/VueBootstrapAutocompleteListItem.vue
@@ -4,6 +4,7 @@
     @keydown.esc.stop.prevent="$emit('listItemBlur')"
     @keydown.down.prevent
     @keydown.up.prevent
+    @keyup.enter="$parent.hitActiveListItem($event)"
     @keyup.down="$parent.selectNextListItem($event)"
     @keyup.up="$parent.selectPreviousListItem($event)"
     @blur="processFocusOut"
